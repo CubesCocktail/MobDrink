67e53a74ab1f3624c397497480f970552060d885
ZamponiMarco <zamponi.marco3@gmail.com>
2021-12-09 17:58:25 +0100

    [UPDATE] Added messages for API interaction

--- mobdrink-core/src/main/java/com/github/zamponimarco/mobdrink/MobDrink.java
+++ mobdrink-core/src/main/java/com/github/zamponimarco/mobdrink/MobDrink.java
@@ -1,7 +1,9 @@
 package com.github.zamponimarco.mobdrink;
 
 import com.github.zamponimarco.cubescocktail.CubesCocktail;
+import com.github.zamponimarco.cubescocktail.action.source.ActionSource;
 import com.github.zamponimarco.cubescocktail.addon.Addon;
+import com.github.zamponimarco.cubescocktail.addon.AddonMessage;
 import com.github.zamponimarco.cubescocktail.libs.command.PluginCommandExecutor;
 import com.github.zamponimarco.cubescocktail.libs.core.Libs;
 import com.github.zamponimarco.mobdrink.command.HelpCommand;
@@ -19,9 +21,13 @@ import com.github.zamponimarco.mobdrink.mob.options.GeneralOptions;
 import com.github.zamponimarco.mobdrink.skill.*;
 import com.github.zamponimarco.mobdrink.spawner.Spawner;
 import lombok.Getter;
+import org.bukkit.Location;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
+import org.bukkit.entity.Entity;
 
 import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class MobDrink extends Addon {
@@ -103,5 +109,32 @@ public class MobDrink extends Addon {
         ex.registerCommand("spawners", new SpawnerListCommand());
         ex.registerCommand("spawn", new MobSpawnCommand());
         CubesCocktail.getInstance().getCommandExecutor().registerCommand("mobs", ex);
+    }
+
+    @Override
+    public <T> T sendMessageToAddon(AddonMessage<T> message) {
+        String command = message.getCommand();
+        List<Object> args = message.getArguments();
+        Mob mob;
+        switch (command) {
+            case "getMobs":
+                return (T) mobManager.getMobs().stream().map(m -> m.getName()).collect(Collectors.toList());
+            case "spawn":
+                mob = mobManager.getByName((String) args.get(1));
+                if (mob == null) return null;
+                return (T) mob.spawn((Location) args.get(0), (Integer) args.get(2), (ActionSource) args.get(3));
+            case "getType":
+                mob = mobManager.getByName((String) args.get(0));
+                if (mob == null) return null;
+                return (T) mob.getType();
+            case "getGUIItem":
+                mob = mobManager.getByName((String) args.get(0));
+                if (mob == null) return null;
+                return (T) mob.getGUIItem();
+            case "getLevel":
+                return (T) (Integer) Mob.getLevel((Entity) args.get(0));
+            default:
+                return null;
+        }
     }
 }
